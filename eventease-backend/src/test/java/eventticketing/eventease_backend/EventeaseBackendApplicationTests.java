5fb4f4afc68c7cb16636cdf593c22d42f8115245
RahaviSiri <rahavi24siri@gmail.com>
2025-07-06 20:29:41 +0530

    Add set up files and connected common db

---
+++ eventease-backend/src/test/java/eventticketing/eventease_backend/EventeaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eventticketing.eventease_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventeaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
